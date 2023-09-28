8388d992c63afb4636cd9ddb698d7a2946833592
Vladislav Mikhaylyuck <39564937+WhiteHodok@users.noreply.github.com>
2023-09-28 22:16:18 +0600

    Features-A1-AddRegister
    - Добавлена регистрация пользователей

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Админ',
-        'title': 'Это первый пост',
-        'content': 'Содержание первого поста.',
-        'date_posted': '28 сентября, 2023'
-    },
-    {
-        'author': 'Пользователь',
-        'title': 'Это второй пост',
-        'content': 'Подробное содержание второго поста.',
-        'date_posted': '28 сентября, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'О нас'})
+    return render(request, 'blog/about.html', {'title': 'О двух разрабах'})
